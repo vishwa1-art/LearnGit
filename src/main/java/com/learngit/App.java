034e992b62000b16bec12bd6b849cb147ee0d2c9
root <maheshwaramshweta1@gmail.com>
2024-12-05 12:10:00 +0530

    changes done to local repository

---
+++ src/main/java/com/learngit/App.java
@@ -0,0 +1,19 @@
+package com.learngit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData(String data) {
+		
+		System.out.println("data is "+data);
+		
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
